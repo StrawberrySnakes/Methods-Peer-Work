a23875f996d88fdb6f20f6c8a417be8da1041c90
Dessa Shapiro <110559444+StrawberrySnakes@users.noreply.github.com>
2024-10-08 10:45:04 -0400

    Added my name and number var and my add function to code

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// This code creates ,ethods for addition and subtraction
+//authors: Dessa Shapiro and Colin 
+namespace MethodsTeam
+{
+    internal class Program
+    {
+        static int Add(int a, int b) {
+            return a + b;
+        
+        }
+        static void Main(string[] args)
+        {
+            int dessaFavoriteNumber;
+            string dessaName = "Dessa";
+
+            //initalize
+            dessaFavoriteNumber = 307;
+
+            //start of our actual program
+            Console.WriteLine("Partner A is "+dessaName+ " and her favoite number is: "+dessaFavoriteNumber);
+
+            //int addedNums = Add(dessaFavoriteNumber, );
+
+        }
+    }
+}
